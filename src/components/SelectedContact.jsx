6a7b9f5ea8f732649dadf8aa6263412089033c83
Kaleab Fekade <kaleab83@gmail.com>
2023-11-29 22:40:14 -0500

    Contact details are rendering

--- src/components/SelectedContact.jsx
+++ src/components/SelectedContact.jsx
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react";
+
 const SelectedContact = ({selectedContactId}) => {
 
-  return <h2>Contact Details for {selectedContactId}</h2>
+  const [contactDetails, setContactDetails] = useState(null);
 
+  useEffect(() => {
+    try {
+    const getDetails = async() => {
+      const response =  await fetch(`https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users/${selectedContactId}`);
+      const data = await response.json();
+      console.log(data);
+      setContactDetails(data);
+    }
+    getDetails();
+  }catch(error) {
+    console.log(error);
+  }
+
+  },[])
+
+  return contactDetails ? (
+    <>
+  <h2>Contact Details for {contactDetails.name}</h2>
+  <h3>Commpany: {contactDetails.company.name}</h3>
+  <h3>Commpany CatchPhrase: {contactDetails.company.catchPhrase}</h3>
+  </> ) : <h2>Loading</h2>
 }
 
 export default SelectedContact;
