ebcc748b74f2d335586cd0a71d7ecb1c23b3a298
Kaleab Fekade <kaleab83@gmail.com>
2023-11-29 20:16:35 -0500

    api call made data rendered

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ContactRow from "./ContactRow";
 
 const dummyContacts = [
@@ -10,6 +10,22 @@ const dummyContacts = [
 const ContactList = () => {
 
   const [contacts, setContacts] = useState(dummyContacts);
+
+  useEffect(() => {
+    try {
+    const fetchContacts = async() => {
+      const response = await fetch(
+        `https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users`
+        );
+      const data = await response.json();
+      setContacts(data);
+    };
+    fetchContacts();
+  }catch(error) {
+    console.log(error);
+  }
+  },[])
+
 
   return ( 
     <table>
