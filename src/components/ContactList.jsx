d912b30f5686ee27d00fd272c3c6cc65412628ff
Kaleab Fekade <kaleab83@gmail.com>
2023-11-29 20:19:49 -0500

    dummy content commented out

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,15 +1,15 @@
 import { useState, useEffect } from "react";
 import ContactRow from "./ContactRow";
 
-const dummyContacts = [
-  { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
-  { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
-  { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
-];
+// const dummyContacts = [
+//   { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
+//   { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
+//   { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
+// ];
 
 const ContactList = () => {
 
-  const [contacts, setContacts] = useState(dummyContacts);
+  const [contacts, setContacts] = useState([]);
 
   useEffect(() => {
     try {
